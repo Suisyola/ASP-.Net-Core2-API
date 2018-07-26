a96f15a3d2b02d6c661b5cb310a8df1900374b3d
AloyDev <tics_zz@hotmail.com>
2018-07-27 00:17:21 +0800

    After completion of Course 05 - Working with Services and Dependency Injection. This covers the following:- 1. Creating and using custom services (MailService) 2. Logging 3. Config files for both Development and Production environment

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -3,31 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CityInfo.API.Controllers
 {
     [Route("api/cities")]
     public class PointsOfInterestController : Controller
     {
+        private ILogger<PointsOfInterestController> _logger;
+        private IMailService _mailService;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger,
+            IMailService mailService)
+        {
+            _logger = logger;
+            _mailService = mailService;
+        }
+
         // GET api/cities/1/pointsOfInterest
         [HttpGet("{cityId}/pointsOfInterest")] 
-        public IActionResult Index(int cityId)
+        public IActionResult GetPointsOfInterest(int cityId)
         {
-            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
-
-            if (city == null)
+            try
             {
-                return NotFound();
-            }
+                var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
 
-            return Ok(city.PointOfInterest);
+                if (city == null)
+                {
+                    // The param in .LogInformation() makes use of String Interpolation. It replaces {cityId} with value.
+                    _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest");
+                    return NotFound();
+                }
+
+                return Ok(city.PointOfInterest);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points of interest for city with id {cityId}.", ex);
+
+                // This error message will return to API consumer, along with HTTP 500. Recommended not to pass stack trace.
+                return StatusCode(500, "A problem happened while handling your request.");
+            }
+            
         }
 
         // GET api/cities/1/pointsOfInterest/2
         [HttpGet("{cityId}/pointsOfInterest/{id}")]
-        public IActionResult Index(int cityId, int id)
+        public IActionResult GetPointsOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
 
@@ -217,6 +242,10 @@ namespace CityInfo.API.Controllers
             }
 
             city.PointOfInterest.Remove(pointOfInterestFromStore);
+
+            // call mail service to send email
+            _mailService.Send("Point of interest deleted.", $"Point of interest {pointOfInterestFromStore.Name} " +
+                                                            $"with id {pointOfInterestFromStore.Id} was deleted.");
 
             return NoContent();
         }
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -2,33 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        
+        // Start of Implementation for usage of configuration. This is for ASP .Net Core 1
+        /*
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            // This block of code is for allowing usage of config file, appSettings.json or appSettings.Production.json, in the application
+            // new ConfigurationBuilder is to create an instance of the Configuration
+            var builder = new ConfigurationBuilder()
+                // indicate where to find settings file
+                .SetBasePath(env.ContentRootPath)
+                // optional:false indicates that appSettings.json is required file
+                // reloadOnChange:true indicates that appSettings.json must be reloaded on change of settings
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                // This adds Production config file. Order is important. If settings appear on both appSettings and appSettings.Production, the value will take from appSettings.Production
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            // Create a configuration and store in the Configuration variable
+            Configuration = builder.Build();
+        }
+        */
+        // End of Implementation for usage of configuration. This is for ASP .Net Core 1
+        
+
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
+                // This allows response type to output application/xml when API consumer wants application/xml in Accept header of request
                 .AddMvcOptions(o => o.OutputFormatters.Add(
                     new XmlDataContractSerializerOutputFormatter()));
-                //.AddJsonOptions(o => {
-                //    if (o.SerializerSettings.ContractResolver != null)
-                //    {
-                //        var castedResolver = o.SerializerSettings.ContractResolver
-                //            as DefaultContractResolver;
-                //        castedResolver.NamingStrategy = null;
-                //    }
-                //});
+            // This outputs JSON variable naming in PascalCase. Without this block of code, the default is camelCase
+            //.AddJsonOptions(o => {
+            //    if (o.SerializerSettings.ContractResolver != null)
+            //    {
+            //        var castedResolver = o.SerializerSettings.ContractResolver
+            //            as DefaultContractResolver;
+            //        castedResolver.NamingStrategy = null;
+            //    }
+            //});
+
+            // add a custom mail service
+            // by using compiler directives, #if etc, application uses different mail service. If using debug build, use LocalMailService.
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService, CloudMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +81,13 @@ namespace CityInfo.API
         {
             // No need to add these loggers in ASP.NET Core 2.0: the call to WebHost.CreateDefaultBuilder(args) 
             // in the Program class takes care of that.
-
             //loggerFactory.AddConsole();
+            //loggerFactory.AddDebug();
 
+            // Use NLog
+            loggerFactory.AddNLog();
+
+            // Display detailed exception page, when the environment in Project properties is set to Development
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -48,6 +97,7 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
+            // Use simple status code page for HTTP response
             app.UseStatusCodePages();
 
             app.UseMvc();
@@ -57,6 +107,7 @@ namespace CityInfo.API
             //    throw new Exception("Example exception");
             //});
 
+            // This will always return Hello World for HTTP response
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
