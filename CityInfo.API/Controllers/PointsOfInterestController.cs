a955d56e2d945cebead0a6fb0270c740ced0058d
AloyDev <tics_zz@hotmail.com>
2018-07-26 00:35:47 -0700

    After completion of Course 04 - Manipulating Resources. This covers the following HTTP verbs POST, PUT, PATCH, DELETE.

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -43,6 +44,181 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        // POST api/cities/1/pointsOfInterest
+        // Name attribute, GetPointOfInterest, is refered in the return statement.
+        [HttpPost("{cityId}/pointsofinterest", Name = "GetPointOfInterest")]
+        public IActionResult CreationPointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            // If cannot deserialise data from Request body into PointOfInterestDtoForCreation
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            // Customised validation logic
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                // Adding error message for customised validation logic
+                ModelState.AddModelError("Description", "Name and Description cannot be the same");
+            }
+
+            // Check again Data Annotation for PointOfInterestForCreationDto, if the model is not valid
+            if (!ModelState.IsValid)
+            {
+                // If there are ErrorMessage set in DTO, need to pass back ModelState. This is to display the ErrorMessage
+                return BadRequest(ModelState);
+            }
+
+            // Get the city to create the point of interest for
+            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
+
+            // If the city cannot be found
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointOfInterest.Add(finalPointOfInterest);
+
+            // This will populate Response header, location, http://localhost:1028/api/cities/3/pointsOfInterest?id=5
+            return CreatedAtRoute("GetPointOfInterest",
+                new { cityId = cityId, id = finalPointOfInterest.Id}, finalPointOfInterest);
+        }
+
+        // PUT api/cities/1/pointsOfInterest/2
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id,
+            [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+            
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "Name and Description cannot be the same");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
+            
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            // HTTP standard indicates that PUT should fully update resource. Hence, API consumer should provide all values for all fields of the resource, except the ID.
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            // Return 204 - No Content. No need to send info about the updated object back to consumer, as they already had the info
+            return NoContent();
+        }
+
+        [HttpPatch("{cityId}/pointsofinterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            // Create a PointOfInterestForUpdateDto object from the domain object to patch
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+            {
+                Name = pointOfInterestFromStore.Name,
+                Description =  pointOfInterestFromStore.Description
+            };
+
+            // Need to pass in Modelstate, so that any validation error flagged from
+            // data annotation would be added to ModelState's error collection
+            // However, this validates JsonPatchDocument(patchDoc) and not pointOfInterestToPatch
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterestToPatch.Description == pointOfInterestToPatch.Name)
+            {
+                ModelState.AddModelError("Description", "Name and Description cannot be the same");
+            }
+            
+            // Validate the object that was patched
+            TryValidateModel(pointOfInterestToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Update domain class if pass validation
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
         }
     }
 }
